8c275b40b3f4bd2f6e1c5152d5fbd1d5c2dcc08e
Ana Berrocal <anaberrocalmail@gmail.com>
2020-10-09 23:57:50 -0400

    Add Bootswatch theme and Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from "./Components/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
       Ana Berrocal - React Developer
       </header>
