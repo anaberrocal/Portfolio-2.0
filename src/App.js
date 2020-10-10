57d7d4aead0156dd32184d02931a2b626a4088fd
Ana Berrocal <anaberrocalmail@gmail.com>
2020-10-10 12:54:04 -0400

    Add content and styles to landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./Components/Navbar";
-import './App.css';
+import Ana from "./assets/Ana_B&W.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <header className="App-header">
-      Hello, my name is Ana Berrocal and I am a React Developer.
+        <div className="intro">
+          <h1 className="intro-title">
+            Hello, my name is Ana Berrocal and I am a React Developer.
+          </h1>
+          <img src={Ana} className="profile-picture" />
+          <div>
+            <button type="button" className="btn btn-primary btn-lg m-5">
+              Learn more
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
