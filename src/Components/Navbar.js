349fb63b758c6bbedc07887806829fe9aca36047
Ana Berrocal <anaberrocalmail@gmail.com>
2020-10-11 12:58:16 -0400

    Add scripts for bootstrap

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar() {
           <li className="nav-item">
             <a className="nav-link" href="#about">
               About 
-              {/* <span className="sr-only">(current)</span> */}
+              <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
